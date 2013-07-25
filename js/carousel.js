15093e7e40d44380f6161149a3f0533aa01b6113
Wesley Carr <surfswell17@gmail.com>
2013-07-25 09:15:52 -0400

    forgot to push to github. need s-show

--- js/carousel.js
+++ js/carousel.js
@@ -1,12 +1,53 @@
 var carousel = document.getElementById('carousel');
+carousel.style.marginLeft = 0;
+
+//Have to add a var margin_left to manipulate the existing img value
+var margin_left = 0;
+
+window.onload = function () {
 
 // Buttons
-var next = document.getElementById('next');
-var previous = document.getElementById('previous');
+  var next = document.getElementById('next');
+  var previous = document.getElementById('previous');
 
-carousel.style.marginLeft = 0;
+  var marginToLeft = function () {
+    margin_left -= 1224;
+    // protection from over-adjustment (612 x 3)
+    margin_left = margin_left % 1836;
+    carousel.style.marginLeft = margin_left + "px";
+  };
+
+  var marginToRight = function () {
+    margin_left -= 612;
+    // protection from over-adjustment (612 x 3)
+    margin_left = margin_left % 1836;
+    carousel.style.marginLeft = margin_left + "px";
+  };
+
+// Create onclick events to advance the images
+  next.onclick = function (event) {
+    marginToRight();
+  };
+
+  previous.onclick = function (event) {
+    marginToLeft();
+  };
 
 // Create mouseoveer and mouseexit events for the buttons
 // to change their opacity from .5 to .2
 
-// Create onclick events to advance the images
+  //next
+  next.onmouseover = function () {
+    this.style.opacity = '0.75';
+  };
+  next.onmouseout = function () {
+    this.style.opacity = '0.5';
+  };
+  //previous
+  previous.onmouseover = function () {
+    this.style.opacity = '0.75';
+  };
+  previous.onmouseout = function () {
+    this.style.opacity = '0.5';
+  };
+};
